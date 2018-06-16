0a76bee02cea828bb9d55ab9a95e84e696210ed9
Isuru Fernando <isuruf@gmail.com>
2018-06-16 02:45:22 -0600

    Add tests

---
+++ .circleci/test_docker_container.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+# check that /opt/conda has correct permissions
+touch /opt/conda/bin/test_conda_forge
+
+# check that conda is activated
+conda info
+
+touch /home/conda/feedstock_root/conda-forge-build-done
