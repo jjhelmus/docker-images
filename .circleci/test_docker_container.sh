ca9d4e20ae1d5d2f49e383a1c7bc4ad812931793
Isuru Fernando <isuruf@gmail.com>
2018-06-16 02:59:33 -0600

    More tests

--- .circleci/test_docker_container.sh
+++ .circleci/test_docker_container.sh
@@ -8,4 +8,7 @@ touch /opt/conda/bin/test_conda_forge
 # check that conda is activated
 conda info
 
+# check that we can install a conda package
+conda install --yes --quiet python -c conda-forge
+
 touch /home/conda/feedstock_root/build_artifacts/conda-forge-build-done
