8459d2e5a19b4f9dfb15f327fb99d9e432b81710
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-02-02 17:36:56 -0500

    Delete run_local.sh

--- run_local.sh
+++
@@ -1,9 +0,0 @@
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
-wget https://github.com/multiarch/qemu-user-static/releases/download/v3.0.0/qemu-ppc64le-static
-chmod +x qemu-ppc64le-static
-
-DOCKERIMAGE=linux-anvil-ppc64le
-DOCKERIMAGE=linux-anvil-aarch64
-
-
-DOCKERIMAGE=linux-anvil-aarch64 docker build -t condaforge/$DOCKERIMAGE -f $DOCKERIMAGE/Dockerfile .
